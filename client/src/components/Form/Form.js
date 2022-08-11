2271cd38301b4fbc93032ea94417f80231efb334
mihaim07 <florynmogo@gmail.com>
2022-08-11 15:52:15 +0300

    Commit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Form.css';
 
 
 
 const Form = () => {
     
+    const [hasSingleProc, setType] = useState("1-3");
+    useEffect(() => {
+        const ip2 = document.getElementById('ip-p2');
+        console.log(ip2)
+        if (hasSingleProc === '4') {
+            ip2.classList.remove('ip-p2-show');
+            ip2.classList.add('ip-p2-hidden');
+        } else {
+            ip2.classList.add('ip-p2-show');
+            ip2.classList.remove('ip-p2-hidden');
+        }
+    });
 
-    
 
     return (
         <div className='form-input'>
             <div>
             <span>
                 <label htmlFor="Type"> Type </label>
-                <select name="Type" id='Type' onChange={selectType}>
+                <select name="Type" id='Type' onChange={ e => setType(e.target.value)}>
                     <option value="1-3">1 / 2 / 3</option>
                     <option value="4">4</option>
                 </select>
@@ -57,18 +68,5 @@ const Form = () => {
     );
 }
 
-var dualProc = document.querySelector("#ip-p2");
-console.log(dualProc);
-
-function selectType(e) {
-    console.log(e)
-    if (e.target.value === "4") {
-        dualProc.classList.add('ip-p2-hidden');
-        console.log('hit here');
-    }
-    else {
-        dualProc.classList.add('ip-p2-show');
-    }
-}
 
 export default Form;
